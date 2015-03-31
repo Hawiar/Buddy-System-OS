8c727e06907d59a194efb9339c91fc7a4d2a185d
joe-proleika <proleikaj6@students.rowan.edu>
2015-03-30 23:16:45 -0400

    Update MemoryTest.java

--- MemoryTest.java
+++ MemoryTest.java
@@ -3,20 +3,23 @@ package memoryMgmtSystem;
 import org.junit.Assert;
 import junit.framework.TestCase;
 
+/*
+ * The JUnit test class for Buddy Memory Management System
+ */
 public class MemoryTest extends TestCase {
 
 	public static void test_add_function () {
 		MemoryObject m = new MemoryObject("start", "add", 7);
 		String[] result = new String[32];
 		for (int i = 0; i < 8; i++) {
-			result[i] = m.getName();
+			result[i] = m.getName(); //Indices 0-7 are "start"
 		}
 		for (int i = 8; i < 32; i++) {
-			result[i] = "empty";
+			result[i] = "empty"; //Indices 8-31 are "empty"
 		}
 		Object[] array = new Object[1];
-		array[0] = m;
-		Assert.assertArrayEquals(result, Core.startMemory(array));
+		array[0] = m; 
+		Assert.assertArrayEquals(result, Core.startMemory(array)); 
 	}
 	
 	public static void test_add_and_remove_function () {
@@ -25,10 +28,10 @@ public class MemoryTest extends TestCase {
 		MemoryObject o = new MemoryObject("start", "remove", 7);
 		String[] result = new String[32];
 		for(int i = 0; i < 32; i++) {
-			result[i] = "empty";
+			result[i] = "empty"; //Indices 0-31 are "empty"
 		}
 		for(int i = 16; i < 32; i++) {
-			result[i] = "middle";
+			result[i] = "middle"; //Indices 16-31 are "middle"
 		}
 		Object[] array = new Object[3];
 		array[0] = m;
